303274de36a31b8090e40869be60d17b0efb52f5
HoMaX <240366321+krossovkaEncore@users.noreply.github.com>
2025-12-23 09:33:25 +0300

    0.1

--- test_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3 
- 
-try: 
-    conn = sqlite3.connect("money_tracker.db") 
-    print("SQLite подключение работает!") 
-    conn.close() 
-except Exception as e: 
-    print(f"Ошибка: {e}") 
