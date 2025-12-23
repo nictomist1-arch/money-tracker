0b535efc1fa5ad3cf74c01de9ba76f9f1549160f
HoMaX <nicto.mist1@gmail.com>
2025-12-23 12:04:24 +0300

    1.2

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,27 @@
-from fastapi import APIRouter
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from jose import JWTError, jwt
+from app.database import get_db
+from app import models, schemas
+from app.config import settings
 
-router = APIRouter(prefix="/auth", tags=["authentication"])
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")  
 
-@router.get("/test")
-def test_auth():
-    return {"message": "Auth router works!"}
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        user_id: int = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        raise credentials_exception
+    return user
